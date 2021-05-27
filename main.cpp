a0f696f2e3c800bf9eccc7f3e2389b8d898f31f0
sjhuang <hsjfans_scholar@163.com>
2021-05-27 09:26:21 +0800

    test error

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
-#include "double_array_tree.h"
+#include "trie/double_array_tree.h"
 #include <iostream>
+using namespace std;
 
-void test(){
+void test()
+{
     DoubleArrayTree *tree = new DoubleArrayTree();
-    vector<string> strs {"apple"};
+    vector<string> strs{"apple"};
     tree->build(strs);
     vector<DoubleArrayTreeNode *> ans = tree->find("apple");
-    if(ans.size() > 0){
+    if (ans.size() > 0)
         cout << ans[0]->c << endl;
-    }
 }
 
-int main(){
+int main()
+{
     test();
     return 0;
 }
